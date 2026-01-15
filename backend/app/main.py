2a8a9eb589e969fba387b165571f1bc768967794
Allan Xavier <xavierfmx2017@gmail.com>
2026-01-15 15:55:46 -0300

    add postgress connection and sqlalchemy engine

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
-from sqlalchemy import text
-from app.db.session import engine
+
 
 app = FastAPI(
     title="Netflix Clone API",
@@ -15,8 +14,5 @@ def health_check():
         "version": "0.1.0"
     }
 
-@app.get("/db-check")
-def db_check():
-    with engine.connect() as conn:
-        result = conn.execute(text("SELECT 1"))
-        return {"db": "connected", "result": result.scalar()}
+
+
